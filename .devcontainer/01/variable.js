425b86d4f232bb540bb385f330b24e93ca8f4539
Ayushkumar9835 <ayushkumar100007@gmail.com>
2025-04-16 21:14:34 +0000

    done

---
+++ .devcontainer/01/variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+ let accountEmail = "hitesh@google.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+//  let accountState; 4Has conversations. Original line has conversations.
+ 
+ // accountId = 2 // not allowed
+ 
+ 
+ accountEmail = "hc@hc.com"
+ accountPassword = "21212121"
+ accountCity = "Bengaluru"
+ 
+ console.log(accountId);
+ 
